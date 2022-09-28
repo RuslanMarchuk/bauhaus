4582d027036e3e4ac4c2ba53d295b3bd95f34d0e
RuslanMarchuk <rusmar@qamadness.com>
2022-09-28 19:21:18 +0200

    pom.xml configuration

---
+++ src/test/java/healthCheck/IncludeMe.java
@@ -0,0 +1,11 @@
+package healthCheck;
+
+import org.testng.annotations.Test;
+
+public class IncludeMe {
+
+    @Test
+    public void includeMe(){
+        System.out.println("Include me");
+    }
+}
